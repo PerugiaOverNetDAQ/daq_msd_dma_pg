db3b7c7b0b57724b3e06a357f272174eb36a9c27
Keida Kanxheri <>
2021-02-12 17:17:23 +0100

    header and footer values modified

--- logic/DAQ_Package.vhd
+++ logic/DAQ_Package.vhd
@@ -5,62 +5,62 @@ use IEEE.std_logic_unsigned.all;
 
 
 package DAQ_Package is
-  
+
   -- Contained in monitor register 0  (abs reg 16)
-  constant Firmware_Version : std_logic_vector(31 downto 0) := x"de100209"; 
-  
+  constant Firmware_Version : std_logic_vector(31 downto 0) := x"de100209";
+
   constant N_MONITOR_REGS : natural := 16; -- Number of mapped monitor registers
   constant N_CONTROL_REGS : natural := 16; -- Number of mapped control registers
-  
+
   -- Types for monitor and control register arrays --
-  type CONTROL_REGS_T is ARRAY (0 to N_CONTROL_REGS-1) of 
+  type CONTROL_REGS_T is ARRAY (0 to N_CONTROL_REGS-1) of
                          STD_LOGIC_VECTOR(31 downto 0);
-  type MONITOR_REGS_T is ARRAY (0 to N_MONITOR_REGS-1) of 
+  type MONITOR_REGS_T is ARRAY (0 to N_MONITOR_REGS-1) of
                          STD_LOGIC_VECTOR(31 downto 0);
-   
-  -- State of the Main Finite State Machine -- 
+
+  -- State of the Main Finite State Machine --
   type MainFSM_state is (Idle, Config, PrepareForRun, Run, EndOfRun, WaitingEmptyFifo);
-   
+
   -- To_State signal Code for Main_FSM --
   constant IdleToConfig : std_logic_vector (1 downto 0) := "00";
   constant ConfigToRun : std_logic_vector (1 downto 0) := "01";
   constant RunToConfig : std_logic_vector (1 downto 0) := "10";
   constant ConfigToIdle : std_logic_vector (1 downto 0) := "11";
-   
+
   -- These are the values used for the header in the Event_Simulator--
-  constant Header1_ES : unsigned (31 downto 0) := x"AAAAAAAA";
+  constant Header1_ES : unsigned (31 downto 0) := x"4EAD0501";
   constant Header1_ES_DUMMY : unsigned (31 downto 0) := x"0ABCDEF0";
-  constant Header2_ES : unsigned (31 downto 0) := x"BBBBBBBB";
-  constant Header3_ES : unsigned (31 downto 0) := x"CCCCCCCC";
-  constant Header4_ES : unsigned (31 downto 0) := x"DDDDDDDD";
+  constant Header2_ES : unsigned (31 downto 0) := x"4EAD1502";
+  constant Header3_ES : unsigned (31 downto 0) := x"4EAD2503";
+  constant Header4_ES : unsigned (31 downto 0) := x"4EAD3504";
   -- These are the values used for the footer in the Event_Simulator --
-  constant Footer1_ES : unsigned (31 downto 0) := x"55555555";
-  constant Footer2_ES : unsigned (31 downto 0) := x"EEEEEEEE";
-  constant Footer3_ES : unsigned (31 downto 0) := x"FFFFFFFF";
+  constant Footer1_ES : unsigned (31 downto 0) := x"F007E501";
+  constant Footer2_ES : unsigned (31 downto 0) := x"F007E502";
+  constant Footer3_ES : unsigned (31 downto 0) := x"F007E503";
   --  Constant part of any word in the random sequence of Event Simulator --
   constant constantPart : unsigned (15 downto 0) := x"1234";
-   
-   
+
+
   -- These are the values used for the header in the Event_Builder --
   constant Header1_EB : unsigned (31 downto 0) := x"EADEBABA";
   signal Header2_Variable : unsigned (31 downto 0) := x"00EADE00";
   -- These are the values used for the footer in the Event_Builder --
   constant Footer1_EB : unsigned (31 downto 0) := x"FAFEFAFE";
   constant Footer2_EB : unsigned (31 downto 0) := x"BACCA000";
-   
+
   -- These are the values used for the header of data read from register file --
   constant Header1_RF : std_logic_vector (31 downto 0) := x"EADE2020";
   constant Header2_RF : std_logic_vector (31 downto 0) := x"EADE2121";
   -- These are the values used for the footer of data coming from register file --
   constant Footer1_RF : std_logic_vector (31 downto 0) := x"FAFEFAFE";
   constant Footer2_RF : std_logic_vector (31 downto 0) := x"BACCA000";
-   
+
   -- Number of cycles LocalEth have to wait before reading the first word of an instruction --
   constant Timeout : natural := 10;
-   
+
   -- Default values for every Register in the register file --
   -- Control Registers --
-  
+
   constant CtrlReg0 : std_logic_vector (31 downto 0) := x"00000000";
   constant CtrlReg1 : std_logic_vector (31 downto 0) := x"00000000";
   constant CtrlReg2 : std_logic_vector (31 downto 0) := x"EADEBECC";
@@ -77,9 +77,9 @@ package DAQ_Package is
   constant CtrlReg13 : std_logic_vector (31 downto 0) := x"00000000";
   constant CtrlReg14 : std_logic_vector (31 downto 0) := x"00000000";
   constant CtrlReg15 : std_logic_vector (31 downto 0) := x"00000000";
-   
+
   -- Monitor Registers --
-   
+
   constant MonReg0 : std_logic_vector (31 downto 0) := Firmware_Version;
   constant MonReg1 : std_logic_vector (31 downto 0) := x"00000000";
   constant MonReg2 : std_logic_vector (31 downto 0) := x"00000000";
@@ -95,8 +95,8 @@ package DAQ_Package is
   constant MonReg12 : std_logic_vector (31 downto 0) := x"00000000";
   constant MonReg13 : std_logic_vector (31 downto 0) := x"00000000";
   constant MonReg14 : std_logic_vector (31 downto 0) := x"00000000";
-  constant MonReg15 : std_logic_vector (31 downto 0) := x"00000000"; 
-  
+  constant MonReg15 : std_logic_vector (31 downto 0) := x"00000000";
+
   -- Default Arrays --
   constant default_MonRegisters : MONITOR_REGS_T := (
   	MonReg0, MonReg1, MonReg2, MonReg3,
@@ -109,7 +109,7 @@ package DAQ_Package is
   	CtrlReg8, CtrlReg9, CtrlReg10, CtrlReg11,
   	CtrlReg12, CtrlReg13, CtrlReg14, CtrlReg15 );
   -- End of Default Values --
-  
+
   ----  Headers of the incoming instrucions  ----
   -- Change FSM State --
   constant Header_ChangeState : std_logic_vector (31 downto 0) := x"EADE0080";
@@ -120,8 +120,8 @@ package DAQ_Package is
   -- Reset Registers of the register file --
   constant Header_ResetRegs : std_logic_vector (31 downto 0) := x"EADE0083";
   -- Footer for the Ethernet instructons --
-  constant Instruction_Footer : std_logic_vector(31 downto 0) := x"F00E0099"; 
-  
+  constant Instruction_Footer : std_logic_vector(31 downto 0) := x"F00E0099";
+
   -- Control Register Map --
   constant Dummy_Reg : natural := 0;
   constant ToState_Reg : natural := 1;
@@ -180,7 +180,7 @@ package DAQ_Package is
   constant FPGA_p_RAM  : natural := 14;
   constant HPS_p_RAM  : natural  := 15;
   --constant Firmware_Version_Reg : natural := 10;
-  
+
   -- GPIO_0 Pin configuration--
   constant Trigger_Pin  : natural := 6;
   constant BCOReset_Pin : natural := 7;
@@ -190,14 +190,14 @@ package DAQ_Package is
   --constant BeamStartOut_Pin : natural := 19;
  -- constant BeamEndOut_Pin   : natural := 21;
   -- The other pins are connected to ground --
-  
+
   -- ADC parameters and definitions
   constant N_CHANNELS : natural := 8; -- Number of channels of the ADC
-  
+
   -- Array containing the values of the ADC
-  type adc_values_t is ARRAY (0 to N_CHANNELS-1) of 
+  type adc_values_t is ARRAY (0 to N_CHANNELS-1) of
                          STD_LOGIC_VECTOR(11 downto 0);
-  
+
   -- Function to convert state --
   function To_stdlogicvector ( Status : MainFSM_state )
                    return std_logic_vector;
@@ -227,7 +227,5 @@ package body DAQ_Package is
       end case;
   		return result;
 	end To_stdlogicvector;
-	
+
 end DAQ_Package;
- 
-	 
